0785b63f0745d21c583bf407f4e3cda39727235e
Ditjo <mads.ditjo@gmail.com>
2026-01-23 14:00:27 +0100

    Added first things to DataManager
    Added first variable to DataManager, funcs to access gold, and emit signal to tell that it has been updated.

--- Globals/Managers/data_manager.gd
+++ Globals/Managers/data_manager.gd
@@ -1,3 +1,20 @@
 extends Node
 
-var test: String
+signal gold_changed(new_amount_gold: int)
+
+var town_name: String
+var _gold: int
+#var _seasons: int
+
+func add_gold(gold: int) -> bool:
+	_gold += gold
+	_on_gold_changed(_gold)
+	return true
+
+func remove_gold(gold: int) -> bool:
+	_gold -= gold
+	_on_gold_changed(_gold)
+	return true
+
+func _on_gold_changed(amount: int) -> void:
+	gold_changed.emit(amount)
