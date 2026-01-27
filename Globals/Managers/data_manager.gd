05a7c27eff7955702093f544cc12d9f4d24d7e20
Elstnet <mikk7867@gmail.com>
2026-01-27 10:49:37 +0100

    creation of mapobject script files + expansion of datamanager

--- Globals/Managers/data_manager.gd
+++ Globals/Managers/data_manager.gd
@@ -4,6 +4,11 @@ signal gold_changed(new_amount_gold: int)
 
 var town_name: String
 var _gold: int
+var _seasons: int
+var _map_objects: Dictionary = {}
+var _structures: Dictionary = {}
+var _population: Array = []
+var _event_flags: Dictionary = {}
 #var _seasons: int
 
 func add_gold(gold: int) -> bool:
@@ -19,6 +24,13 @@ func remove_gold(gold: int) -> bool:
 func _on_gold_changed(amount: int) -> void:
 	gold_changed.emit(amount)
 
+func get_gold() -> int:
+	return _gold
+
+func increase_seasons() -> void:
+	_seasons += 1
+	#TODO: update ui or call ui update - signal
+	
 
 #When a MapObject or Building is added/deleted it needs to emit a signal telling what and where it was. 
 #The TileMapLayer script will then sub to that and add/delete it.
