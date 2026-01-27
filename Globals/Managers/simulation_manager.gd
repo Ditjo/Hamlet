ea106a85b2dfb2baf86a6e5d7633cb113da77818
Elstnet <mikk7867@gmail.com>
2026-01-27 14:13:01 +0100

    basic code structure in simulation_manager

--- Globals/Managers/simulation_manager.gd
+++ Globals/Managers/simulation_manager.gd
@@ -2,3 +2,25 @@ extends Node
 
 #Skal indholde simulationen af spillet. Game Loop. 
 #Skal subscribe til et signal fra GameManageren som fortæller hvornår at simuleringen skal køre. 
+
+func sim_main() -> void:
+	event_func()
+	harvest_func()
+	production_func()
+	sale_func()
+	people_func()
+
+func event_func() -> void:
+	pass
+
+func harvest_func() -> void:
+	pass
+
+func production_func() -> void:
+	pass
+
+func sale_func() -> void:
+	pass
+
+func people_func() -> void:
+	pass
