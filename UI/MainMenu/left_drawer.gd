7f0a946a965fed371b778f0be9a5a1aae87d4962
Ditjo <mads.ditjo@gmail.com>
2026-01-27 12:23:13 +0100

    minor tweeks in MainMenu
    minor tweeks in MainMenu

--- UI/MainMenu/left_drawer.gd
+++ UI/MainMenu/left_drawer.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 @onready var drawer := self
 var is_open: bool = true
 var drawer_width: int = 300
+@export var animation_time: float = 0.25
 
 func _ready() -> void:
 	CheatManager.toggle_left_drawer.connect(toggle)
@@ -12,10 +13,10 @@ func toggle():
 	is_open = !is_open
 	
 	if is_open:
-		tween.tween_property(drawer, "position:x", 0, 0.25)\
+		tween.tween_property(drawer, "position:x", 0, animation_time)\
 		.set_trans(Tween.TRANS_QUAD)\
 		.set_ease(Tween.EASE_OUT)
 	else:
-		tween.tween_property(drawer, "position:x", -drawer_width, 0.25)\
+		tween.tween_property(drawer, "position:x", -drawer_width, animation_time)\
 		.set_trans(Tween.TRANS_QUAD)\
 		.set_ease(Tween.EASE_OUT)
--- UI/MainMenu/right_drawer.gd
+++ UI/MainMenu/right_drawer.gd
@@ -3,6 +3,7 @@ extends PanelContainer
 @onready var drawer := self
 var is_open: bool = false
 var drawer_width: int = 300
+@export var animation_time: float = 0.25
 
 func _ready() -> void:
 	CheatManager.toggle_right_drawer.connect(toggle)
@@ -13,10 +14,10 @@ func toggle():
 	is_open = !is_open
 	
 	if is_open:
-		tween.tween_property(drawer, "position:x", get_viewport_rect().size.x - drawer_width, 0.25)\
+		tween.tween_property(drawer, "position:x", get_viewport_rect().size.x - drawer_width, animation_time)\
 		.set_trans(Tween.TRANS_QUAD)\
 		.set_ease(Tween.EASE_OUT)
 	else:
-		tween.tween_property(drawer, "position:x", get_viewport_rect().size.x, 0.25)\
+		tween.tween_property(drawer, "position:x", get_viewport_rect().size.x, animation_time)\
 		.set_trans(Tween.TRANS_QUAD)\
 		.set_ease(Tween.EASE_OUT)
