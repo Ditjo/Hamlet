0a06e9541dff201276152046b31607fd770a2c8e
Ditjo <mads.ditjo@gmail.com>
2026-01-27 11:00:33 +0100

    Updated Enums
    Added all the Enums

--- Globals/Enums/enums.gd
+++ Globals/Enums/enums.gd
@@ -1,1 +1,35 @@
 extends Node
+
+enum MapObjectTypes {
+	STRUCTURES,
+	TREES,
+	ROCKS,
+	MOUNTAINS,
+	WATER,
+	ROAD
+}
+
+enum CostTypes {
+	GOLD
+}
+
+enum StructureTypes {
+	HOUSE,
+	FIELD,
+	WINDMILL
+}
+
+enum EventFlags {
+	LORDISSATISFIED,
+	LOARDISVENGEFULL,
+	DUCKTALES,
+	BROKENWINDMILL
+}
+
+enum EventTypes {
+	EVENT,
+	HARVEST,
+	PRODUCTION,
+	SALE,
+	POPULATION
+}
