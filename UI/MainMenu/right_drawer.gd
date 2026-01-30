a072f160676b13385bdb244a5c811e8fcc755453
Ditjo <97234116+Ditjo@users.noreply.github.com>
2026-01-30 14:23:00 +0100

    Main Menu Draws function done
    Main Menu draws can now go in and out and scale with window size

--- UI/MainMenu/right_drawer.gd
+++ UI/MainMenu/right_drawer.gd
@@ -2,22 +2,37 @@ extends PanelContainer
 
 @onready var drawer := self
 var is_open: bool = false
-var drawer_width: int = 300
 @export var animation_time: float = 0.25
+@export_range(0.0, 0.5, 0.05, "How far left draw slides out in %")
+var drawer_width_ratio: float = 0.25
 
 func _ready() -> void:
 	CheatManager.toggle_right_drawer.connect(toggle)
-	position.x = get_viewport_rect().size.x
+	get_viewport().size_changed.connect(_on_window_resized)
+	_on_window_resized()
+
+func get_drawer_width() -> float:
+	return get_window().size.x * drawer_width_ratio
+
+func _on_window_resized() -> void:
+	drawer.size.x = get_drawer_width()
+	if is_open:
+		drawer.position.x = get_window().size.x - get_drawer_width()
+	else:
+		drawer.position.x = get_window().size.x
 
 func toggle(): 
 	var tween: Tween = create_tween()
-	is_open = !is_open
+	var width: float = 0
 	
+	is_open = !is_open
 	if is_open:
-		tween.tween_property(drawer, "position:x", get_viewport_rect().size.x - drawer_width, animation_time)\
-		.set_trans(Tween.TRANS_QUAD)\
-		.set_ease(Tween.EASE_OUT)
+		#Open Drawer
+		width = get_window().size.x - get_drawer_width()
 	else:
-		tween.tween_property(drawer, "position:x", get_viewport_rect().size.x, animation_time)\
-		.set_trans(Tween.TRANS_QUAD)\
-		.set_ease(Tween.EASE_OUT)
+		#Close Drawer
+		width = get_window().size.x
+	
+	tween.tween_property(drawer, "position:x", width, animation_time)\
+	.set_trans(Tween.TRANS_QUAD)\
+	.set_ease(Tween.EASE_OUT)
