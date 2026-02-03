b0074e520640eeccec11c37b72982005d56b216e
Ditjo <97234116+Ditjo@users.noreply.github.com>
2026-02-03 11:46:05 +0100

    RightDrawer can now be controled
    Right Drawers can be controled by how heigh and  where the are placed.

--- UI/Shared/right_drawer.gd
+++ UI/Shared/right_drawer.gd
@@ -4,8 +4,14 @@ class_name RightDrawer
 @onready var drawer := self
 var is_open: bool = false
 @export var animation_time: float = 0.25
-@export_range(0.0, 0.5, 0.05, "How far left draw slides out in %")
+@export_range(0.0, 0.5, 0.05)
 var drawer_width_ratio: float = 0.25
+@export_range(0.0, 1.0, 0.05)
+var drawer_height_ratio: float = 1.0
+@export_range(0.0, 1.0, 0.05)
+var drawer_vertical_anchor: float = 0.5
+#Placement of Drawer Vertical. 0: Top, 0.5: Center, 1.0: Bottom
+
 
 func _ready() -> void:
 	self.anchor_left = 1
@@ -20,12 +26,22 @@ func _ready() -> void:
 func get_drawer_width() -> float:
 	return get_window().size.x * drawer_width_ratio
 
+func get_drawer_height() -> float:
+	return get_window().size.y * drawer_height_ratio
+
 func _on_window_resized() -> void:
 	drawer.size.x = get_drawer_width()
+	drawer.size.y = get_drawer_height()
+	
+	# Horizontal Position
 	if is_open:
 		drawer.position.x = get_window().size.x - get_drawer_width()
 	else:
 		drawer.position.x = get_window().size.x
+	
+	# Vertical Position
+	var free_space = get_window().size.y - get_drawer_height()
+	drawer.position.y = free_space * drawer_vertical_anchor
 
 func close()-> void:
 	if is_open:
