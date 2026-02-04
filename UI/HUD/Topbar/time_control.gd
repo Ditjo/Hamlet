d6e6da68717f6ef977b55f49c1cea787e6142213
Ditjo <97234116+Ditjo@users.noreply.github.com>
2026-02-04 10:01:14 +0100

    Added simple Pause btn
    Added Pause btn, that chnages "Icon" and tooltip depending on state. Still need to call Pause func in GameManager.

--- UI/HUD/Topbar/time_control.gd
+++ UI/HUD/Topbar/time_control.gd
@@ -1,9 +1,26 @@
 extends PanelContainer
 
 @onready var season_count: BasicInfoPanel = $HBoxContainer/SeasonCount
+@onready var pause_play_btn: Button = $HBoxContainer/HBoxContainer/PausePlayBtn
+
+var is_paused: bool = false
 
 func _ready() -> void:
 	DataManager.seasons_changed.connect(_set_seasons)
+	pause_play_btn.pressed.connect(_pause_player_action)
 
 func _set_seasons(value: int) -> void:
 	season_count.set_amount(value)
+
+func _pause_player_action() -> void:
+	is_paused = !is_paused
+	
+	if is_paused:
+		pause_play_btn.text = "II"
+		pause_play_btn.tooltip_text = "The Game is Paused"
+		#Call Method In GameManager to Pause
+	else:
+		pause_play_btn.text = ">"
+		pause_play_btn.tooltip_text = "The Game Playing"
+		#Call Method In GameManager to UnPause
+		
