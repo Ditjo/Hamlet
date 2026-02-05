55c620ec9e4e6e5530da07231b2bab6f88e0ffbe
Elstnet <mikk7867@gmail.com>
2026-02-05 10:50:58 +0100

    pause functionality fixed

--- Globals/Managers/game_manager.gd
+++ Globals/Managers/game_manager.gd
@@ -2,8 +2,8 @@ extends Node
 
 var is_running := false
 var is_paused := false
-"""
 var was_paused := false
+"""
 signal paused
 signal unpaused
 """
@@ -15,31 +15,48 @@ func _init():
 func game_main():
 	is_running = true
 	is_paused = false
+	was_paused = false
 	run_game_loop()
 	pass
 
 func run_game_loop():
 	while is_running:
-		await get_tree().create_timer(5.0).timeout
-		if get_tree().paused:
-			await wait_until_unpaused()
+		await logical_waiter(5.0)
+		#if get_tree().paused:
+			#await wait_until_unpaused()
 		#evt small timeout el lign her for stagger mellem pause og gamefunctions
-		print("skadoosh")
+		print("running")
 		#await SimulationManager.sim_main()
 		#put season/ui stuff here?
 
-func wait_until_unpaused():
-	while get_tree().paused:
+func logical_waiter(sec: float, buffer: float = 0.5):
+	var remaining := sec
+	while remaining > 0.0:
 		await get_tree().process_frame
+		if is_paused:
+			continue
+		if was_paused:
+			remaining = max(remaining, buffer)
+			was_paused = false
+		remaining -= get_process_delta_time()
+
+#func wait_until_unpaused():
+	#while get_tree().paused:
+		#await get_tree().process_frame
 
 func pause_game():
-	if get_tree().paused:
+	if is_paused:
 		return
-	get_tree().paused = true
+	#get_tree().paused = true
 	#paused.emit()
+	print("paused")
+	is_paused = true
+	was_paused = true
 
 func unpause_game():
-	if not get_tree().paused:
+	if not is_paused:
 		return
-	get_tree().paused = false
+	#get_tree().paused = false
 	#unpaused.emit()
+	print("unpaused")
+	is_paused = false
