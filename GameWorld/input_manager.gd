66b70813f6f525918e6f7756a455ed4870fe570e
Ditjo <97234116+Ditjo@users.noreply.github.com>
2026-02-05 09:57:43 +0100

    Fixed Coords problem
    Was using the wrong position, in -unhandled_input(). Now using get_local_mouse_position().
    Also fixed som spelling.

--- GameWorld/input_manager.gd
+++ GameWorld/input_manager.gd
@@ -2,16 +2,16 @@ extends Node2D
 
 #TileMapLayers
 @onready var background_layer: TileMapLayer = $"../TileMaps/BackgroundLayer"
-@onready var natur_layer: TileMapLayer = $"../TileMaps/NaturLayer"
+@onready var nature_layer: TileMapLayer = $"../TileMaps/NatureLayer"
 @onready var structure_layer: TileMapLayer = $"../TileMaps/StructureLayer"
 
-#Tile Infor Panel
+#Tile Info Panel
 @onready var tile_info_drawer: Drawer = %TileInfoDrawer
 @onready var tile_info_panel: TileInfoPanel = %TileInfoPanel
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.is_pressed():
-		var mouse_pos: Vector2 = event.position
+		var mouse_pos: Vector2 = get_local_mouse_position()
 		match event.button_index:
 			MOUSE_BUTTON_LEFT:
 				handle_tile_click(mouse_pos)
@@ -19,24 +19,29 @@ func _unhandled_input(event: InputEvent) -> void:
 				_close_tile_info_drawer()
 
 func handle_tile_click(mouse_pos: Vector2) -> void:
+	#Layers might get out of sync with each other. 
+	#If that is the case, need to rework how the to check if things exist on a layer
 	var local_pos = background_layer.to_local(mouse_pos)
 	var tile_coords = background_layer.local_to_map(local_pos)
 	
 	check_layer(tile_coords)
 
 func check_layer(coords: Vector2i) -> void:
+	print("structure_layer: " + str(structure_layer.get_cell_source_id(coords)))
 	if structure_layer.get_cell_source_id(coords) != -1:
 		#Handle Tile on structure_layer Layer
 		print("structure_layer: ", coords)
 		_open_tile_info_drawer("structure_layer" + str(coords))
 		return
-		
-	if natur_layer.get_cell_source_id(coords) != -1:
+	
+	print("nature_layer: " + str(nature_layer.get_cell_source_id(coords)))
+	if nature_layer.get_cell_source_id(coords) != -1:
 		#Handle Tile on natur_layer Layer
 		print("natur_layer: ", coords)
 		_open_tile_info_drawer("natur_layer" + str(coords))
 		return
-	
+		
+	print("background_layer: " + str(background_layer.get_cell_source_id(coords)))
 	if background_layer.get_cell_source_id(coords) != -1:
 		#Handle Tile on background_layer Layer
 		print("background_layer: ", coords)
