727568fb92d9adc0a180b4cd5799c8d4ae475284
Ditjo <97234116+Ditjo@users.noreply.github.com>
2026-02-06 11:19:20 +0100

    Added some Method to popup

--- UI/HUD/EventPopUp/event_pop_up.gd
+++ UI/HUD/EventPopUp/event_pop_up.gd
@@ -1,5 +1,28 @@
 extends PopupPanel
 
+signal button_pressed(button: String)
+
 func _ready() -> void:
 	#self.anchor
+	
 	pass
+	
+		
+func _on_button_pressed(button: String) -> void:
+	button_pressed.emit(button)
+	close()
+
+func close() -> void:
+	self.hide()
+
+func open(event: Event) -> void:
+	#event.options
+	"foreach option in options"
+	"build buttons"
+	pass
+	self.popup()
+	"""
+	
+	
+	
+	"""
