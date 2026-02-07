382f95106d5fb8f9a307a0d9681d58dae262bea5
Ditjo <97234116+Ditjo@users.noreply.github.com>
2026-02-07 11:04:33 +0100

    EventPopup done for now
    Eventpopup is done for now, but there are some problems that needs to be fixed.

--- Events/Individual/Bad_Harvest.gd
+++ Events/Individual/Bad_Harvest.gd
@@ -5,7 +5,10 @@ func _init(
 	name_: String = "Bad Harvest",
 	desc_: String = "It's been a bad year for the Harvest.",
 	type_: Enums.EventTypes = Enums.EventTypes.HARVEST,
-	opts_: Array[Option] = [Option.new("Sad, but nothing we can do", "ok")]
+	opts_: Array[Option] = [
+		Option.new("Sad, but nothing we can do", "sad"),
+		Option.new("Okay, but nothing we can do", "ok")
+	]
 ) -> void:
 	event_name = name_
 	description = desc_
--- UI/HUD/EventPopUp/event_pop_up.gd
+++ UI/HUD/EventPopUp/event_pop_up.gd
@@ -4,12 +4,14 @@ class_name EventPopup
 
 signal button_pressed(button: String)
 
-@onready var event_title: Label = $VBoxContainer/EventTitle
-@onready var description: RichTextLabel = $VBoxContainer/Description
-@onready var options_panel: HBoxContainer = $VBoxContainer/OptionsPanel
+@onready var event_title: Label = $MarginContainer/VBoxContainer/EventTitle
+@onready var description: RichTextLabel = $MarginContainer/VBoxContainer/Description
+@onready var options_panel: HBoxContainer = $MarginContainer/VBoxContainer/OptionsPanel
+
+var is_visible: bool = false
 
 func _ready() -> void:
-	get_window().size_changed.connect(_on_window_resize)
+	get_tree().root.get_window().size_changed.connect(_on_window_resize)
 	_cleanup()
 
 func _on_button_pressed(response: String) -> void:
@@ -18,6 +20,7 @@ func _on_button_pressed(response: String) -> void:
 
 func close() -> void:
 	self.hide()
+	is_visible = false
 	_cleanup()
 	
 func open(event: Event) -> void:
@@ -25,12 +28,23 @@ func open(event: Event) -> void:
 	event_title.text = event.event_name
 	description.text = event.description
 	for o in event.options:
+		#MakeBtn
 		var btn = Button.new()
 		btn.text = o.btn_text
+		btn.custom_minimum_size.x = 100
 		btn.pressed.connect(_on_button_pressed.bind(o.response))
-		options_panel.add_child(btn)
+		
+		#MakeContainer
+		var center = CenterContainer.new()
+		center.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+		center.size_flags_vertical = Control.SIZE_SHRINK_CENTER
+		
+		center.add_child(btn)
+		
+		options_panel.add_child(center)
 	
 	_on_window_resize()
+	is_visible = true
 	self.popup()
 
 func _cleanup() -> void:
@@ -41,25 +55,22 @@ func _cleanup() -> void:
 		btn.queue_free()
 
 func _on_window_resize() -> void:
-	_popup_percent(0.6, 0.4)
+	
+	_popup_percent(0.4, 0.4)
+	
+	if is_visible:
+		call_deferred("popup", size)
 
 func _popup_percent(width_pct: float, height_pct: float):
-	#var window_size = get_window().get_visible_rect().size
-	#
-	#var popup_size = Vector2(
-		#window_size.x * width_pct,
-		#window_size.y * height_pct
-	#)
+	var main_window: Window = get_tree().root.get_window()
+	var window_size: Vector2i = main_window.size
 	
-	size.x = 600
-	size.y = 400
+	var popup_size = Vector2(
+		window_size.x * width_pct,
+		window_size.y * height_pct
+	)
 	
-	#size.x = get_window().window_size.x * width_pct
-	#size.y = get_window().window_size.y * height_pct
-	
-	#size = popup_size
-	
-	#position = (get_window().size - popup_size) * 0.5
+	size = popup_size
 	
 	
 	
