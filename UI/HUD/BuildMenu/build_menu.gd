a854955f55e78d0d4497e9e92e17130e50b433ca
Ditjo <97234116+Ditjo@users.noreply.github.com>
2026-02-09 09:09:49 +0100

    Added some code to buildmenu
    Added some code to buildmene. Not in use yet but will be.

--- UI/HUD/BuildMenu/build_menu.gd
+++ UI/HUD/BuildMenu/build_menu.gd
@@ -1,9 +1,9 @@
 extends PanelContainer
 class_name Buildmenu
 
-@onready var house_btn: Button = $HScrollBar/HBoxContainer/HouseBtn
-@onready var field_btn: Button = $HScrollBar/HBoxContainer/FieldBtn
-@onready var windmill_btn: Button = $HScrollBar/HBoxContainer/WindmillBtn
+@onready var house_btn: Button = $HBoxContainer/HouseBtn
+@onready var field_btn: Button = $HBoxContainer/FieldBtn
+@onready var windmill_btn: Button = $HBoxContainer/WindmillBtn
 
 signal building_selected(type: Enums.StructureTypes)
 
@@ -14,3 +14,39 @@ func _ready() -> void:
 	
 func _on_btn_pressed(type: Enums.StructureTypes) -> void:
 	building_selected.emit(type)
+
+func _get_structures() -> Array[GDScript]:
+	var structures: Array[GDScript] = []
+	
+	#Opens the directory on the path and null checks
+	var dir: = DirAccess.open("res://World/Structures/")
+	if dir == null:
+		return structures
+	
+	#begins to Look into the folder and takes the next/first one
+	dir.list_dir_begin()
+	var folder: = dir.get_next()
+	
+	#So long as folder is not empty, loop
+	while folder != "":
+		if dir.current_is_dir() and not folder.begins_with("."):
+			#Opens Subfolder and check if there is anything
+			var sub: = DirAccess.open("res://World/Structures/%s" % folder)
+			if sub:
+				sub.list_dir_begin()
+				var file: = sub.get_next()
+				
+				#Runs through subfolders files
+				while file != "":
+					if file.ends_with(".gd"):
+						structures.append(load("res://World/Structures/%s/%s" % [folder, file]))
+					file = sub.get_next()
+		
+		folder = dir.get_next()
+	
+	dir.list_dir_end()
+	return	structures
+	
+
+func _populate_build_menu() -> void:
+	pass
